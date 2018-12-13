5f2286d3550957e411dbc8d7af6bd566e6d834cc
cameronmcphie <quiksilver980@gmail.com>
2018-12-12 22:17:04 -0800

    Initial Commit

---
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeed.kt
@@ -0,0 +1,12 @@
+package edu.fullerton.ecs.cpsc411.miniapp4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculateTransferSpeed : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_transfer_speed)
+    }
+}
---
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeedViewModel.kt
@@ -0,0 +1,15 @@
+package edu.fullerton.ecs.cpsc411.miniapp4
+
+import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableDouble
+import java.util.*
+
+class CalculateTransferSpeedViewModel : ViewModel() {
+    var fileSize  = ""
+    var networkSpeed = ""
+    var transferSpeed = ""
+
+    fun calcSpeed() {
+
+    }
+}
