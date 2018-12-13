e8e10260de3650f2638b52f0669ab0ac6ba06a8b
cameronmcphie <quiksilver980@gmail.com>
2018-12-13 01:23:40 -0800

    It's working!

--- app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeed.kt
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeed.kt
@@ -1,12 +1,24 @@
 package edu.fullerton.ecs.cpsc411.miniapp4
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import edu.fullerton.ecs.cpsc411.miniapp4.CalculateTransferSpeedViewModel
+import edu.fullerton.ecs.cpsc411.miniapp4.databinding.ActivityCalculateTransferSpeedBinding
+
 
 class CalculateTransferSpeed : AppCompatActivity() {
 
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(CalculateTransferSpeedViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calculate_transfer_speed)
+
+        val binding : ActivityCalculateTransferSpeedBinding = DataBindingUtil.setContentView(this, R.layout.activity_calculate_transfer_speed)
+
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeedViewModel.kt
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/miniapp4/CalculateTransferSpeedViewModel.kt
@@ -2,14 +2,21 @@ package edu.fullerton.ecs.cpsc411.miniapp4
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableDouble
-import java.util.*
 
 class CalculateTransferSpeedViewModel : ViewModel() {
     var fileSize  = ""
     var networkSpeed = ""
-    var transferSpeed = ""
+    var transferSpeed = ObservableDouble()
 
-    fun calcSpeed() {
+    fun onSpeedOnChanged() {
+        if (fileSize != "" && networkSpeed != "") {
+            var fs = fileSize.toDouble()
+            var ns = networkSpeed.toDouble()
 
+            fs *= Math.pow(2.0, 20.0) * 8.0
+            ns *= Math.pow(10.0, 6.0)
+
+            transferSpeed.set(fs / ns)
+        }
     }
 }
